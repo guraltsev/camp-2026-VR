fc3f080b589cb938cf62032368429c8fdc0eac12
Gennady Uraltsev <gennady.uraltsev@gmail.com>
2026-06-22 10:53:50 -0600

    octahedron enriched

--- src/examples/octahedron.world.js
+++ src/examples/octahedron.world.js
@@ -79,5 +79,59 @@ marker = traffic_cone("octahedron-marker", {
   turn: -18,
 });
 
-OnFace("face-0", [centerpiece]);
-OnFace("face-4", [marker]);
+face_0_flowers = flower_pot("octahedron-face-0-flower-pot", {
+  position: [3.2, 0, -1.2],
+  scale: 0.85,
+  turn: 18,
+});
+
+face_1_tree = tree("octahedron-face-1-tree", {
+  position: [0.8, 0, -0.8],
+  scale: 0.9,
+  turn: -22,
+});
+
+face_2_bench = bench("octahedron-face-2-bench", {
+  position: [-0.4, 0, 1.2],
+  scale: 0.8,
+  turn: 42,
+});
+
+face_3_flower_group = flower_group("octahedron-face-3-flower-group", {
+  position: [0.6, 0, -0.4],
+  scale: 0.95,
+  turn: -14,
+});
+
+face_4_stop_sign = stop_sign("octahedron-face-4-stop-sign", {
+  position: [2.5, 0, -1.3],
+  scale: 0.75,
+  turn: 26,
+});
+
+face_5_bicycle = bicycle("octahedron-face-5-bicycle", {
+  position: [-0.7, 0, 0.6],
+  scale: 0.75,
+  turn: -38,
+});
+
+face_6_grass = grass("octahedron-face-6-grass", {
+  position: [0.2, 0, -0.8],
+  scale: 1.1,
+  turn: 12,
+});
+
+face_7_tree_swirl = tree_swirl("octahedron-face-7-tree-swirl", {
+  position: [0.4, 0, 0.1],
+  scale: 0.9,
+  turn: 31,
+});
+
+OnFace("face-0", [centerpiece, face_0_flowers]);
+OnFace("face-1", [face_1_tree]);
+OnFace("face-2", [face_2_bench]);
+OnFace("face-3", [face_3_flower_group]);
+OnFace("face-4", [marker, face_4_stop_sign]);
+OnFace("face-5", [face_5_bicycle]);
+OnFace("face-6", [face_6_grass]);
+OnFace("face-7", [face_7_tree_swirl]);
